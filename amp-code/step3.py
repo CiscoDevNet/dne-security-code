993bdff959a012c3e9f24a2e91c5b3b9ac08beb8
veeratcisco <veer@cisco.com>
2018-09-13 11:49:13 -0700

    Changes made to make people work on the code

--- amp-code/step3.py
+++ amp-code/step3.py
@@ -1,10 +1,42 @@
+#!/usr/bin/env python
+"""0day Workflow Mission - edit this file
+This is your starting point for the 0day workflow  Mission.
+Edit this file to
+ - 
+There are a few places to edit (search for MISSION comments)
+
+Script Dependencies:
+    requests
+Depencency Installation:
+    $ pip install requests
+Copyright (c) 2018, Cisco Systems, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 import requests
 import json
-
+# Disable SSL Certificate warning
+try:
+    requests.packages.urllib3.disable_warnings()
+except:
+    pass
 #function definitions
 def get(url):
 	try:
-	    response = requests.get(url)
+	    response = requests.get(url, verify=False)
 	    # Consider any status other than 2xx an error
 	    if not response.status_code // 100 == 2:
 	        return "Error: Unexpected response {}".format(response)
@@ -20,9 +52,14 @@ def get(url):
 client_id = ""
 api_key = ""
 
+#TODO: Enter the specific event you are interested in to find from the result for example malware execute event id is 1107296272
+event_id=
 
-events_url = "https://{}:{}@api.amp.cisco.com/v1/events".format(client_id,api_key)
+events_url = "https://{}:{}@amp.dcloud.cisco.com/v1/events".format(client_id,api_key)
 
-events= get(events_url)
+events1= get(events_url)
 
-print events
+#TODO: Print the entire response
+
+#TODO: Print the events where Malware executed. You will be using For loop to parse the json in the response
+
