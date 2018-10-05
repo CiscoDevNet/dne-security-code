6725765c2497c65a1bc801cf26fc24958a3bcbee
Chris Lunsford <chrlunsf@cisco.com>
2018-10-05 18:54:24 -0400

    Remove fmc-specific environment file

--- dne-security-fmc-code/fmc_environment.py
+++
@@ -1,9 +0,0 @@
-"""DevNet Express FMC lab environment."""
-
-# Providing hostnames and access credentials in your source code is okay for
-# learning, not for production.  We recommend getting information like this
-# from the environment, CLI input, or from a configuration file.
-FMC_HOST = "10.19.66.126"
-FMC_PORT = "40003"
-FMC_USER = "apiuser"
-FMC_PASSWORD = "C1sc0123"
