cde568ab0889f2223115c63373682dd486f756be
Chris Lunsford <chrlunsf@cisco.com>
2019-04-10 18:35:13 -0400

    Update amp-mission.py

--- intro-amp/mission/amp-mission.py
+++ intro-amp/mission/amp-mission.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
-"""0day Workflow Mission - edit this file
-This is your starting point for the 0day workflow  Mission.
-Edit this file to
- -
-There are a few places to edit (search for MISSION comments)
+"""Zero-day Workflow Mission - Cisco AMP
 
-Script Dependencies:
-    requests
-Depencency Installation:
-    $ pip install requests
-Copyright (c) 2019-2020 Cisco and/or its affiliates.
+This is your starting point for the Zero-day Workflow Mission.  There are a few
+places where you will need to do some work (search for MISSION comments).
+
+
+Copyright (c) 2018-2019 Cisco and/or its affiliates.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,104 +26,124 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import webexteamssdk
+
 import json
-import os
 import sys
-import requests
-from pprint import pprint
 from pathlib import Path
 
-# Get the absolute path for the directory where this file is located
+import requests
+import webexteamssdk
+from crayons import blue, green, red
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+# Locate the directory containing this file and the repository root.
+# Temporarily add these directories to the system path so that we can import
+# local files.
 here = Path(__file__).parent.absolute()
+repository_root = (here / ".." / "..").resolve()
 
-# Get the path for the project / repository root
-project_root = (here/"../../").resolve()
+sys.path.insert(0, str(repository_root))
 
-# Extend the system path to include the project root and import the env files
-sys.path.insert(0, str(project_root))
-
-import env_lab      # noqa
-import env_user     # noqa
-
-# Disable Certificate warning
-try:
-    requests.packages.urllib3.disable_warnings()
-except:
-    pass
-from crayons import green
-
-#Intialize the webexteam SDK
-
-teams = webexteamssdk.WebexTeamsAPI(env_user.WEBEX_TEAMS_ACCESS_TOKEN)
-
-# Read the config file evn_user.py to get the AMP API Key settings
-client_id = env_user.AMP_CLIENT_ID
-api_key = env_user.AMP_API_KEY
-
-print(f""" {green("My AMP Lab Environment:")} 
-AMP Host: {env_lab.AMP["host"]} 
- """) 
-
-# Function for the GET Request to the AMP
-def getAMP(url):
-	try:
-	    response = requests.get(url, verify=False)
-	    # Consider any status other than 2xx an error
-	    if not response.status_code // 100 == 2:
-	        return "Error: Unexpected response {}".format(response)
-	    try:
-	        return response.json()
-	    except:
-	        return "Error: Non JSON response {}".format(response.text)
-	except requests.exceptions.RequestException as e:
-	    # A serious problem happened, like an SSLError or InvalidURL
-	    return "Error: {}".format(e)
+import env_lab  # noqa
+import env_user  # noqa
 
 
-#Enter the standard AMP event id for type of event for Malware... it is 1107296272
-event_id = "1107296272"
-
-# Grab the AMP hostname from the lab_evn.py 
-host=env_lab.AMP["host"]
-
-#  create the URL for the AMP request 
-events_url = "https://{}:{}@{}/v1/events".format(client_id,api_key, host)
-
-events1 = getAMP(events_url)
+# Disable insecure request warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-#Let intialize containers datastructs for different data we will collect from AMP
-sha_list= {}
-iplist=[]
-maclist=[]
+# Functions
+def get_amp_events(
+    host=env_lab.AMP.get("host"),
+    client_id=env_user.AMP_CLIENT_ID,
+    api_key=env_user.AMP_API_KEY,
+):
+    """Get a list of recent events from Cisco AMP."""
+    print("\n==> Getting recent events from AMP")
 
-#Just in case you want to debug uncomment out the print statement
+    url = f"https://{client_id}:{api_key}@{host}/v1/events"
 
-#print (json.dumps(events1, indent=4, sort_keys=True))
-for events1 in events1["data"]:
-	if events1["event_type_id"] == 1107296272:
-		sha_list[events1["computer"]["hostname"]] = json.dumps(events1["file"]["identity"]) + "\n IP: " + events1["computer"]["network_addresses"][0]["ip"] + "\n Mac: " + events1["computer"]["network_addresses"][0]["mac"]
-		iplist.append(events1["file"]["identity"]["sha256"])
-		maclist.append(events1["computer"]["network_addresses"][0]["mac"])
-	else:
-		continue
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
 
-# In the begining of the file we established the project root directory
-wearehere = project_root
+    events_list = response.json()["data"]
 
-# We will write the Mac address list of endpoints where malware executed to a file, 
-# ISE Mission script will read this file and nuke these endpoints in quarantine. 
-with open(os.path.join(wearehere,"mission-data", "macaddr.txt"), "w") as file:
-	file.write(json.dumps(maclist))
-file.close()
-# We will write the malware SHA list to the file, to be used in ThreatGrid Mission
-with open(os.path.join(wearehere, "mission-data", "sha.txt"), "w") as file:
-	file.write(json.dumps(iplist))
-file.close()
+    print(green(f"Retrieved {len(events_list)} events from AMP"))
 
-#Finally post the message in the DevNet Express Teams Room and Brag!!!
+    return events_list
 
-teams.messages.create(env_user.WEBEX_TEAMS_ROOM_ID, text="AMP Mission completed!!!")
-print(green("AMP Mission Completed!!!"))
-	
+
+def extract_observables(amp_events):
+    """Extract hash, IP, and MAC address observables from Malware events."""
+    print(blue("\n==> Extracting observables from the AMP events"))
+
+    # Initialize data structures for collecting observables
+    observables = []
+
+    # Standard AMP event ID for Malware events
+    malware_event_id = "1107296272"
+
+    for event in amp_events:
+        if event["event_type_id"] == malware_event_id:
+            hostname = event["computer"]["hostname"]
+            ip = event["computer"]["network_addresses"][0]["ip"]
+            mac = event["computer"]["network_addresses"][0]["mac"]
+            sha256 = event["file"]["identity"]["sha256"]
+
+            observables.append({
+                "hostname": hostname,
+                "ip_address": ip,
+                "mac_address": mac,
+                "sha256": sha256,
+            })
+
+    if observables:
+        print(green(f"Extracted observables from "
+                    f"{len(observables)} malware events"))
+    else:
+        print(red("No malware events found."))
+        sys.exit(1)
+
+    return observables
+
+
+# If this script is the "main" script, run...
+if __name__ == "__main__":
+    # Get the list of events from AMP
+    amp_events = get_amp_events()
+
+    # Extract the Malware event observables
+    amp_observables = extract_observables(amp_events)
+
+    # Save the MAC addresses of the endpoints where malware executed to a JSON
+    # file.  In the ISE Mission we will read this file and quarantine these
+    # endpoints.
+    macaddr_path = repository_root / "mission-data/macaddr.json"
+    print(blue(f"\n==> Saving MAC address observables to: {macaddr_path}"))
+
+    with open(macaddr_path, "w") as file:
+        mac_addresses = [o["mac_address"] for o in amp_observables]
+        json.dump(mac_addresses, file, indent=2)
+
+    # Save the malware SHA256 hashes to a JSON file. We will use these in the
+    # ThreatGrid Mission.
+    sha256_path = repository_root / "mission-data/macaddr.json"
+    print(blue(f"\n==> Saving SHA256 hash observables to: {sha256_path}"))
+
+    with open(sha256_path, "w") as file:
+        sha256_hashes = [o["sha256"] for o in amp_observables]
+        json.dump(sha256_hashes, file, indent=2)
+
+    # Finally, post a message to the Webex Teams Room to brag!!!
+    print(blue("\n==> Posting message to Webex Teams"))
+
+    teams = webexteamssdk.WebexTeamsAPI(env_user.WEBEX_TEAMS_ACCESS_TOKEN)
+    teams.messages.create(
+        roomId=env_user.WEBEX_TEAMS_ROOM_ID,
+        markdown=f"**AMP Mission completed!!!**\n"
+                 f"I extracted observables from {len(amp_observables)} AMP"
+                 f"malware events."
+    )
+
+    print(green("AMP Mission Completed!!!"))
