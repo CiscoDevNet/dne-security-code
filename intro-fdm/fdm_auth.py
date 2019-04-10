d0565583dd41419d681bcd69ce59c039aa757d49
Chris Lunsford <chrlunsf@cisco.com>
2019-04-10 14:25:13 -0400

    Update FDM scripts

---
+++ intro-fdm/fdm_auth.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+"""Authenticate with FDM and obtain an API access token.
+
+Copyright (c) 2018-2019 Cisco and/or its affiliates.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+import sys
+from pathlib import Path
+
+import requests
+from crayons import blue, green
+from requests import HTTPError
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+# Locate the directory containing this file and the repository root.
+# Temporarily add these directories to the system path so that we can import
+# local files.
+here = Path(__file__).parent.absolute()
+repository_root = (here / "..").resolve()
+
+sys.path.insert(0, str(repository_root))
+
+from env_lab import FDM  # noqa
+
+
+# Disable insecure request warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def fdm_login(
+    host=FDM.get("host"),
+    port=FDM.get("port"),
+    username=FDM.get("username"),
+    password=FDM.get("password"),
+):
+    """Login to FDM and return an access token that may be used for API calls.
+
+    This is login will give you an access token that is valid for ~30 minutes
+    with no refresh. Using this token should be fine for short running scripts.
+
+    Do not use for services that need to last longer than 30 minutes.
+    """
+    print(blue("\n==> Logging into FDM and requesting an access token"))
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+
+    payload = {
+        "grant_type": "password",
+        "username": username,
+        "password": password,
+    }
+
+    response = requests.post(
+        f"https://{host}:{port}/api/fdm/v1/fdm/token",
+        headers=headers,
+        json=payload,
+        verify=False,
+    )
+
+    try:
+        response.raise_for_status()
+        access_token = response.json()["access_token"]
+
+    except HTTPError:
+        if response.status_code == 400:
+            raise HTTPError(f"Error logging in to FDM: {response.text}")
+        else:
+            raise
+
+    except ValueError:
+        raise ValueError("Error parsing the response from FDM")
+
+    print(green("Login was successful!"))
+    print(f"Access Token: {access_token}")
+
+    return access_token
+
+
+# If this script is the "main" script, run...
+if __name__ == "__main__":
+    token = fdm_login()
---
+++ intro-fdm/fdm_create_network.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+"""Get the list of all networks in FDM.
+
+Copyright (c) 2018-2019 Cisco and/or its affiliates.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+import sys
+from pathlib import Path
+from pprint import pformat
+
+import requests
+from crayons import blue, green, white
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+# Locate the directory containing this file and the repository root.
+# Temporarily add these directories to the system path so that we can import
+# local files.
+here = Path(__file__).parent.absolute()
+repository_root = (here / "..").resolve()
+
+sys.path.insert(0, str(repository_root))
+sys.path.insert(0, str(here))
+
+from env_lab import FDM  # noqa
+from fdm_auth import fdm_login  # noqa
+
+
+# Data for the network object to be created
+NETWORK_OBJECT = {
+    "name": "PUT_NAME_HERE",
+    "description": "DevNet Security",
+    "subType": "HOST",
+    "value": "5.5.5.5",
+    "type": "networkobject"
+}
+
+
+# Disable insecure request warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def fdm_create_network(
+    network_object,
+    access_token,
+    host=FDM.get("host"),
+    port=FDM.get("port"),
+):
+    """Create a new network in FDM."""
+    print(blue("\n==> Creating a new network in FDM"))
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    payload = network_object
+
+    response = requests.post(
+        f"https://{host}:{port}/api/fdm/v1/object/networks",
+        headers=headers,
+        json=payload,
+        verify=False,
+    )
+    response.raise_for_status()
+
+    print(green("Successfully created the new network!"))
+    return response.json()
+
+
+# If this script is the "main" script, run...
+if __name__ == "__main__":
+    token = fdm_login()
+    new_network_details = fdm_create_network(NETWORK_OBJECT, token)
+
+    print(
+        f"{white('Network Details:', bold=True)}",
+        f"{pformat(new_network_details)}",
+        sep="\n"
+    )
