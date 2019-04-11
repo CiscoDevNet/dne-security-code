4c109dd5037f228f8cad02beb83fe94c088300c8
veeratcisco <veer@cisco.com>
2019-04-11 11:57:24 -0700

    added

--- intro-amp/amp_custom_detect.py
+++ intro-amp/amp_custom_detect.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
-"""AMP-CODE - edit this file
-This is your starting point for the 0day workflow  Mission.
-Edit this file to
- -
-There are a few places to edit (search for MISSION comments)
-
-Script Dependencies:
-    requests
-Depencency Installation:
-    $ pip install requests
+"""
+Intro to Cisco AMP Step 3 
 Copyright (c) 2018-2019 Cisco and/or its affiliates.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,44 +21,86 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import requests
 import json
-# Disable SSL Certificate warning
-try:
-    requests.packages.urllib3.disable_warnings()
-except:
-    pass
-#BONOS TODO: You can create the function definitions to do GET and POST operations
+import sys
+from pathlib import Path
 
-#main code TODO: ENTER YOU CLIENT ID AND API KEY HERE
-client_id = "1512e5b0c0c2f2b85401"
-api_key = "eaef340f-0ccd-46a5-bcd3-dd62dcbdfb02"
+import requests
+import webexteamssdk
+from crayons import blue, green, red
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-sha_256="3372c1edab46837f1e973164fa2d726c5c6f17dcb888828ccd7c4dfcc234a375"
 
-#TODO: Create the URL /v1/file_lists/simple_custom_detections is the api endpoint.
+# Locate the directory containing this file and the repository root.
+# Temporarily add these directories to the system path so that we can import
+# local files.
+here = Path(__file__).parent.absolute()
+repository_root = (here / ".." ).resolve()
 
-file_list_url = "https://{}:{}@amp.dcloud.cisco.com/v1/file_lists/simple_custom_detections".format(client_id,api_key)
+sys.path.insert(0, str(repository_root))
 
-#First do the GET request to find the custom blacklist file exists
-response = requests.get(file_list_url, verify=False)
-print(file_list_url)
-file_lists= response.json()
-#TODO: Parse the response json, and find the "File Blacklist"
-print(file_lists)
-for item in file_lists["data"]:
-    if item["name"] == "File Blacklist":
-        list_id = item["guid"]
+import env_lab  # noqa
+import env_user  # noqa
 
-print(list_id)
-#Create the payload to update the list with new SHA
-post_this =  {'description':'created by DNE user using API'}
-#TODO: Create the POST URL and Add sha to it
-add_list_url = "https://{}:{}@amp.dcloud.cisco.com/v1/file_lists/{}/files/{}".format(client_id, api_key,list_id, sha_256)
-print(add_list_url)
-print(json.dumps(post_this))
-#TODO: Now you have URL figured out, Do your POST to update the SHA in the blacklist.
-response = requests.post(add_list_url, post_this, verify=False)
-#add_item = post(add_list_url, json.dumps(post_this))
+SAMPLE_SHA256="3372c1edab46837f1e973164fa2d726c5c7f17dcb888828ccd7c4dfcc234a375"
 
-print(response)
+# Disable insecure request warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+# Functions
+def get_amp_detections(
+    host=env_lab.AMP.get("host"),
+    client_id=env_user.AMP_CLIENT_ID,
+    api_key=env_user.AMP_API_KEY
+):
+    """Get a list of recent custom detections from Cisco AMP."""
+    print("\n==> Getting recent custom detections from AMP")
+
+    url = f"https://{client_id}:{api_key}@{host}/v1/file_lists/simple_custom_detections"
+
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+
+    events_list = response.json()
+
+    print(green(f"Retrieved {len(events_list)} data from AMP"))
+
+    return events_list
+
+def parseResponse(file_lists):
+    print(file_lists)
+    for item in file_lists["data"]:
+        if item["name"] == "File Blacklist":
+            list_id = item["guid"]
+    return list_id
+    
+def post_to_amp(
+    list_id,
+    payload,
+    host=env_lab.AMP.get("host"),
+    client_id=env_user.AMP_CLIENT_ID,
+    api_key=env_user.AMP_API_KEY):
+    print("\n==> Adding  SHA to AMP custome detections list")
+
+    url = f"https://{client_id}:{api_key}@{host}/v1/file_lists/{list_id}/files/{SAMPLE_SHA256}"
+
+    response = requests.post(url, post_this, verify=False)
+    response.raise_for_status()
+
+    rdata = response.json()["data"]
+    return rdata
+
+# If this script is the "main" script, run...
+if __name__ == "__main__":
+
+    #TODO: Call the function to get detections
+    dects = get_amp_detections()
+    #TODO: Call the function to parse the custome detections list
+    list_id=parseResponse(dects)
+    post_this =  {'description':'created by DNE user using API'}
+    #TODO: Call post function to add a problem sha to your custom detection list 
+    response = post_to_amp(list_id, post_this)
+    
+    print(f"\n==> Successfully added "{SAMPLE_SHA256}" to AMP custom detections list")
+
--- intro-amp/amp_query_param.py
+++ intro-amp/amp_query_param.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
-"""AMP-CODE - edit this file
-This is your starting point for the 0day workflow  Mission.
-Edit this file to
- -
-There are a few places to edit (search for MISSION comments)
-
-Script Dependencies:
-    requests
-Depencency Installation:
-    $ pip install requests
+"""
+Intro to Cisco AMP Step 3 
 Copyright (c) 2018-2019 Cisco and/or its affiliates.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,35 +21,62 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import requests
+
+
 import json
-# Disable SSL Certificate warning
-try:
-    requests.packages.urllib3.disable_warnings()
-except:
-    pass
-#function definitions
-def get(url):
-    try:
-        response = requests.get(url, verify=False)
-        # Consider any status other than 2xx an error
-        if not response.status_code // 100 == 2:
-            return "Error: Unexpected response {}".format(response)
-        try:
-            return response.json()
-        except:
-            return "Error: Non JSON response {}".format(response.text)
-    except requests.exceptions.RequestException as e:
-        # A serious problem happened, like an SSLError or InvalidURL
-        return "Error: {}".format(e)
+import sys
+from pathlib import Path
 
-#main code TODO: ENTER YOU CLIENT ID AND API KEY HERE
-client_id = ""
-api_key = ""
+import requests
+import webexteamssdk
+from crayons import blue, green, red
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-#TODO: Enter the URL with Filter applied for the event type and limit
-events_url = "https://{}:{}@api.amp.cisco.com/v1/events?event_type[]=1090519054&limit=10".format(client_id,api_key)
 
-events= get(events_url)
+# Locate the directory containing this file and the repository root.
+# Temporarily add these directories to the system path so that we can import
+# local files.
+here = Path(__file__).parent.absolute()
+repository_root = (here / ".." ).resolve()
 
-print(events)
+sys.path.insert(0, str(repository_root))
+
+import env_lab  # noqa
+import env_user  # noqa
+
+
+# Disable insecure request warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+# Functions
+def get_amp_events(
+    qparams,
+    host=env_lab.AMP.get("host"),
+    client_id=env_user.AMP_CLIENT_ID,
+    api_key=env_user.AMP_API_KEY
+):
+    """Get a list of recent events from Cisco AMP."""
+    print("\n==> Getting recent events from AMP")
+
+    url = f"https://{client_id}:{api_key}@{host}/v1/events"
+
+    response = requests.get(url,params=qparams, verify=False)
+    response.raise_for_status()
+
+    events_list = response.json()["data"]
+
+    print(green(f"Retrieved {len(events_list)} events from AMP"))
+
+    return events_list
+
+
+# If this script is the "main" script, run...
+if __name__ == "__main__":
+    #TODO: Create the query for AMP here
+    qparams = "event_type[]=1090519054&limit=10"
+    #TODO: Call the function to get events
+    amp_events = get_amp_events(qparams)
+    #TODO: Print the events 
+    print(json.dumps(amp_events, indent=2))
+
