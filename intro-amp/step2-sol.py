61eb075c3c5cdd6ae55058fa0af5cb3ff48911ee
veeratcisco <veer@cisco.com>
2019-04-11 08:55:08 -0700

    changes for step2-sol

--- intro-amp/step2-sol.py
+++ intro-amp/step2-sol.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
-"""AMP-CODE - edit this file
-This is your starting point for the 0day workflow  Mission.
-Edit this file to
- -
-There are a few places to edit (search for MISSION comments)
+"""
+Cisco Intro to AMP Hands on Exercisce Step2
 
-Script Dependencies:
-    requests
-Depencency Installation:
-    $ pip install requests
 Copyright (c) 2018-2019 Cisco and/or its affiliates.
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,38 +22,56 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import requests
+
+
 import json
-# Disable SSL Certificate warning
-try:
-    requests.packages.urllib3.disable_warnings()
-except:
-    pass
-#function definitions
-def get(url):
-    try:
-        response = requests.get(url, verify=False)
-        # Consider any status other than 2xx an error
-        if not response.status_code // 100 == 2:
-            return "Error: Unexpected response {}".format(response)
-        try:
-            return response.json()
-        except:
-            return "Error: Non JSON response {}".format(response.text)
-    except requests.exceptions.RequestException as e:
-        # A serious problem happened, like an SSLError or InvalidURL
-        return "Error: {}".format(e)
+import sys
+from pathlib import Path
 
-#main code TODO: ENTER YOU CLIENT ID AND API KEY HERE
-client_id = ""
-api_key = ""
-
-#TODO: Enter the URL
-
-events_url = "https://{}:{}@amp.dcloud.cisco.com/v1/events".format(client_id,api_key)
-
-event_ids= get(event_id_url)
+import requests
+import webexteamssdk
+from crayons import blue, green, red, yellow
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 
-print(event_ids)
+# Locate the directory containing this file and the repository root.
+# Temporarily add these directories to the system path so that we can import
+# local files.
+here = Path(__file__).parent.absolute()
+repository_root = (here / ".." ).resolve()
+
+sys.path.insert(0, str(repository_root))
+
+import env_lab  # noqa
+import env_user  # noqa
+
+
+# Disable insecure request warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+# Functions
+def get_amp_events(
+    host=env_lab.AMP.get("host"),
+    client_id=env_user.AMP_CLIENT_ID,
+    api_key=env_user.AMP_API_KEY,
+):
+    """Get a list of recent events from Cisco AMP."""
+    print("\n==> Getting recent events from AMP")
+    #TODO: Construct the URL
+    url = f"https://{client_id}:{api_key}@{host}/v1/events"
+
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+
+    events_list = response.json()["data"]
+    #TODO: Return the event list
+    return events_list
+
+# If this script is the "main" script, run...
+if __name__ == "__main__":
+    #TODO: Get the list of events from AMP
+    amp_events = get_amp_events()
+    #TODO: Print the total number of events observed. 
+    print(yellow(f"Received total {len(amp_events)} malware events"))
 
