c071ac77fd0180427d74f329196ebfee5c200ee2
veeratcisco <veer@cisco.com>
2019-05-09 00:26:51 -0700

    updated to v1.1

--- intro-umbrella/InvestigateGetRequest.py
+++ intro-umbrella/InvestigateGetRequest.py
@@ -1,4 +1,55 @@
-# SOLUTION SECTION #2 GET REQUEST LAB 5-HandsOn-Investigate-API-Hunting
+##!/usr/bin/env python
+"""
+Copyright (c) 2018-2019 Cisco and/or its affiliates.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from datetime import datetime
+import requests
+import socket
+import configparser
+import json
+import sys
+from pathlib import Path
+import webexteamssdk
+from crayons import blue, green, red
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+# Locate the directory containing this file and the repository root.
+# Temporarily add these directories to the system path so that we can import
+# local files.
+here = Path(__file__).parent.absolute()
+repository_root = (here / ".." ).resolve()
+
+sys.path.insert(0, str(repository_root))
+
+sys.path.insert(0, str(repository_root))
+
+from env_lab import UMBRELLA  # noqa
+from env_user import UMBRELLA_INVESTIGATE_KEY  # noqa
+# Disable insecure request warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+# SOLUTION SECTION #2 POST REQUEST LAB 5-HandsOn-Investigate-API-Hunting
 
 # import necessary libraries / modules
 import requests
@@ -6,10 +57,13 @@ import json
 from datetime import datetime
 
 # copy paste API key from previous section within the quotes
-investigate_api_key = "<insert-investigate-api-key-here>"
+investigate_api_key = UMBRELLA_INVESTIGATE_KEY
 
 # URL needed for the domain status and category
-investigate_url = "https://investigate.api.umbrella.com/domains/categorization/"
+# investigate_url = "https://investigate.api.umbrella.com/domains/categorization/"
+inv_u = UMBRELLA.get("inv_url")
+investigate_url = f"{inv_u}/domains/categorization/" 
+
 
 # domain that will be checked
 domain = "internetbadguys.com"
