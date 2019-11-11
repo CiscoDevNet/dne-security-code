a18b1252621e337f2f2a6aecc71aed62b4e7bf8c
Krishan Veer (veer) <veer@cisco.com>
2019-11-11 14:44:03 -0800

    Matched it with LL

--- intro-threatgrid/threatgrid_ioc_query.py
+++ intro-threatgrid/threatgrid_ioc_query.py
@@ -98,12 +98,7 @@ if __name__ == "__main__":
     query_start = datetime.utcnow() - timedelta(days=TIME_PERIOD)
     query_end = datetime.utcnow()
     #TODO Call the function for query feed.
-    malware_samples = query_threatgrid_ioc_feed(
-        query_domain,
-        after=f"{query_start.isoformat()}Z",
-        before=f"{query_end.isoformat()}Z",
-    )
-
+    malware_samples = 
     malware_samples_path = here / f"{query_domain}-malware-samples-data.json"
     print(blue(f"\n==> Saving samples data to: {malware_samples_path}"))
     #TODO: Pass the proper path to the open function
