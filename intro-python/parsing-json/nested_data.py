bdb7bf785e995a25eaa3b7a64df828ea30277b39
Brian Sak <briansak@users.noreply.github.com>
2021-01-13 06:40:31 -0600

    Update nested_data.py

--- intro-python/parsing-json/nested_data.py
+++ intro-python/parsing-json/nested_data.py
@@ -10,9 +10,9 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-with open(os.path.join(here, "interfaces.json")) as file:
+with open(os.path.join(here, "computers.json")) as file:
     # TODO: Parse the contents of the JSON file into a variable
 
 
 # TODO: Loop through the interfaces in the JSON data and print out each
-# interface's name, ip, and netmask.
+# computers's name, ip, and detected malware.
